cfe24c5ae6f38188a577ea8e861463855e0a64ec
Ryan Roussel <rroussel@slac.stanford.edu>
2026-04-30 14:05:32 -0500

    create factory method for creating models

---
+++ virtual_accelerator/models/facet2.py
@@ -0,0 +1,53 @@
+from virtual_accelerator.bmad.factory import BmadModelSpec, build_bmad_model
+
+def get_facet_bmad_model(
+    start_element="PROF241", end_element="END", track_beam=False, custom_beam_path=None
+):
+    """
+    Get the LUMEBmadModel for the FACET-II lattice from PROF241 to END.
+
+    Parameters
+    -------------
+    start_element: str, optional
+        The starting element for the model. Default is "PROF241".
+    end_element: str, optional
+        The ending element for the model. Default is "END".
+    track_beam: bool, optional
+        Whether to enable beam tracking in the model. Default is False.
+    custom_beam_path: str, optional
+        Path to custom beam file for tracking. If None, will use default design beam. Default is None.
+
+
+    Returns
+    -------
+    LUMEBmadModel
+        Instance of the LUMEBmadModel for the FACET-II lattice.
+    """
+
+    spec = BmadModelSpec(
+        feature="FACET-II Bmad model",
+        lattice_env_var="FACET2_LATTICE",
+        tao_init_relpath="bmad/models/f2_elec/tao.init",
+        mapping_beampath="F2_ELEC",
+        screens=("PR10241", "PR10571", "PR10771"),
+        profmon_config_filename="facet2_profmon_info.yaml",
+    )
+    return build_bmad_model(
+        spec=spec,
+        start_element=start_element,
+        end_element=end_element,
+        track_beam=track_beam,
+        custom_beam_path=custom_beam_path,
+    )
+
+
+def get_facet2_model(
+    start_element="PROF241", end_element="END", track_beam=False, custom_beam_path=None
+):
+    """Backward-compatible alias for FACET-II BMAD model construction."""
+    return get_facet_bmad_model(
+        start_element=start_element,
+        end_element=end_element,
+        track_beam=track_beam,
+        custom_beam_path=custom_beam_path,
+    )
